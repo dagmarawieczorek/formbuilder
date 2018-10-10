c27797eb50386f77bd682f758e023ed57838ec75
dagmarawieczorek <dagmara.sylwia.w@gmail.com>
2018-10-10 13:20:24 +0200

    made elements - input and subinput

--- js/app.jsx
+++ js/app.jsx
@@ -3,29 +3,70 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    class AddForm extends React.Component {
+    class Input extends React.Component {
 
+        render() {
+            return <div className="all-forms">
+            <div className="form-box">
+                <div className="form-description">
+                    <span> Question</span>
+                    <span>Type</span>
+                </div>
+                <form className="form-inputs">
+                    <input type="text" className="form-input"/>
+                    <select name="Yes/No" className="form-input">
+                        <option value="Yes/No">Yes/No</option>
+                        <option value="Text">Text</option>
+                        <option value="Number">Number</option>
+                    </select>
+                    <div className="form-buttons">
+                    <button>Add Sub-Input</button>
+                    <button>Delete</button>
+                    </div>
+                </form>
+            </div>
+                <SubInput>
+                    <SubInput></SubInput>
+                </SubInput>
+
+            </div>;
+        }
+    }
+
+
+
+    class SubInput extends React.Component {
 
         render() {
 
-            return <div>
-
-                <div className="form-box">
-                    <div className="form-description">
-                        <span> Question</span>
-                        <span>Type</span>
-                    </div>
-                    <form>
-                        <input type="text"/>
-                        <select name="Yes/No">
-                            <option value="Yes">Yes</option>
-                            <option value="No">No</option>
+            return <div className="all-forms">
+             <div className="form-box ">
+                <div className="form-description">
+                    <span> Condition</span>
+                    <span> Question</span>
+                    <span>Type</span>
+                </div>
+                <form className="form-inputs">
+                    <div className="condition">
+                        <select name="Equals" className="form-input">
+                            <option value="equals">Equals</option>
+                            <option value="greater">Greater than</option>
                         </select>
+                        <input type="text" className="form-input"/>
+                    </div>
+                    <input type="text" className="form-input"/>
+                    <select name="Yes/No" className="form-input">
+                        <option value="Yes/No">Yes/No</option>
+                        <option value="Text">Text</option>
+                        <option value="Number">Number</option>
+                    </select>
+                    <div className="form-buttons">
                         <button>Add Sub-Input</button>
                         <button>Delete</button>
-                    </form>
-                </div>
-
+                    </div>
+                </form>
+             </div>
+                {this.props.children}
             </div>;
 
         }
@@ -33,10 +74,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
+
     class App extends React.Component {
         render() {
             return <div>
-                <AddForm/>
+                <Input/>
 
             </div>;
         }
