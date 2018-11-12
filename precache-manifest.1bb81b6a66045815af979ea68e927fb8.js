ecd943ada1fd2da671de4bc3d526bff48cacc360
dagmarawieczorek <dagmara.sylwia.w@gmail.com>
2018-11-12 16:34:10 +0100

    Updates

---
+++ precache-manifest.1bb81b6a66045815af979ea68e927fb8.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e86a9eb12c51400f642",
+    "url": "/formbuilder/static/js/runtime~main.8e86a9eb.js"
+  },
+  {
+    "revision": "f6da12ea617ce037c225",
+    "url": "/formbuilder/static/js/main.f6da12ea.chunk.js"
+  },
+  {
+    "revision": "3b5dc610a35d1b964633718af7ef2ca8",
+    "url": "/formbuilder/index.html"
+  }
+];
